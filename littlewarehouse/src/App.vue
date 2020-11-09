f307617557f4b5a744160d65d950b6ad46732bf6
Amanda <amanda.chou@frog-jump.com>
2020-11-09 20:52:22 +0800

    vue add router

--- littlewarehouse/src/App.vue
+++ littlewarehouse/src/App.vue
@@ -1,68 +1,32 @@
 <template>
   <div id="app">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an 
-      </p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <div>{{title}}</div>
-      <el-form :model="model" :rules="rules" ref="loginForm">
-      <el-form-item label="姓名" prop="username">
-        <el-input v-model="model.username" autocomplete="off" placeholder="請輸入帳號"></el-input>
-      </el-form-item>
-      <el-form-item label="確認密碼" prop="password">
-          <el-input type="password" v-model="model.password" autocomplete="off" placeholder="請輸入密碼"></el-input>
-          <!-- type="password" 字型是密碼的星星樣式-->
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('loginForm')">Submit</el-button>
-        </el-form-item>
-    </el-form>
-  
-      <div>
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-      </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  },
-  props:{
-   // title:String
-  },
-  //一次性的頁面資料初始化 constructor
-  data() {
-    return {
-      title: "請先登錄",
-      value: "", //接收子Input事件onInput傳來的value
-      model: { username: "", password: "" }, //說白了就是數據
-      rules: {
-        username: [
-          { required: true, message: "請輸入姓名" },
-          { min: 6, max: 10, message: "請輸入6至10位姓名" }
-        ],
-        password: [{ required: true, message: "請輸入密碼" }]
-      }
-    }; //return要帶上;
-  },
-}
-
-</script>
-
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- littlewarehouse/src/main.js
+++ littlewarehouse/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import './plugins/element.js'
 import ElementUI from 'element-ui';
 import'element-ui/lib/theme-chalk/index.css';
+import router from './router'
 
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
