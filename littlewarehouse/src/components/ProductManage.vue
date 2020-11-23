55926686a2713e3e421423f07d14d37112021353
Amanda <amanda.chou@frog-jump.com>
2020-11-23 19:59:11 +0800

    商品管理頁面

--- littlewarehouse/src/components/ProductManage.vue
+++ littlewarehouse/src/components/ProductManage.vue
@@ -1,93 +1,95 @@
 <template>
-  <div>
-    <h2>商品管理</h2>
-    <el-row>
-      <el-col :span="24"
-        ><div class="grid-content bg-purple-dark">
-         
-          <br />
-          <span>商品名稱</span>
-          <el-select v-model="value" placeholder="Select">
+    <div> <h1>商品管理</h1>
+        <el-select v-model="selectedValue" placeholder="Select" @change="myclick">
             <el-option
-              v-for="item in cities"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            >
-              <span style="float: left">{{ item.label }}</span>
-              <span style="float: right; color: #8492a6; font-size: 13px">{{
-                item.value
-              }}</span>
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
             </el-option>
-          </el-select>
-          <br />
-          <br /></div
-      ></el-col>
-    </el-row>
-  </div>
+        </el-select>
+    <hr>
+
+      <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+        <el-table-column
+        prop="date"
+        label="商品"
+        width="180">
+        </el-table-column>
+        <el-table-column
+        prop="name"
+        label="地區"
+        width="180">
+        </el-table-column>
+        <el-table-column
+        prop="address"
+        label="數量">
+        </el-table-column>
+         <el-table-column
+            fixed="right"
+            label="Operations"
+            width="120">
+        <el-button
+          @click.native.prevent="deleteRow(scope.$index, tableData)"
+          type="text"
+          size="small"
+          class="el-icon-shopping-cart-full">          
+        </el-button>
+     </el-table-column>
+
+    </el-table>
+    
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      cities: [
-        {
-          value: "Beijing",
-          label: "Beijing",
-        },
-        {
-          value: "Shanghai",
-          label: "Shanghai",
-        },
-        {
-          value: "Nanjing",
-          label: "Nanjing",
-        },
-        {
-          value: "Chengdu",
-          label: "Chengdu",
-        },
-        {
-          value: "Shenzhen",
-          label: "Shenzhen",
-        },
-        {
-          value: "Guangzhou",
-          label: "Guangzhou",
-        },
-      ],
-      value: "",
-    };
-  },
-};
+    name:'product',
+    data() {
+        return {
+        options: [{
+          value: '商品1',
+          label: '我是商品1'
+        }, {
+          value: '商品2',
+          label: '我是商品2'
+        }, {
+          value: '商品3',
+          label: '我是商品3'
+        }, {
+          value: '商品4',
+          label: '我是商品4'
+        }, {
+          value: '商品5',
+          label: '我是商品5'
+        }],
+        selectedValue: '',
+        tableData: [{
+          date: '產品-03',
+          name: '台北',
+          address: '21'
+        }, {
+          date: '產品-02',
+          name: '高雄',
+          address: '23'
+        }, {
+          date: '產品-04',
+          name: '台北',
+          address: '11'
+        }, {
+          date: '產品-01',
+          name: '台中',
+          address: '16'
+        }]
+        }
+    }, 
+    methods : {
+        myclick() {
+            alert(this.selectedValue)
+        }
+    }
+}
 </script>
-
-<style>
-.el-row {
-  margin-bottom: 20px;
-  &:last-child {
-    margin-bottom: 0;
-  }
-}
-.el-col {
-  border-radius: 4px;
-}
-.bg-purple-dark {
-  background: #99a9bf;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
-}
-.grid-content {
-  border-radius: 4px;
-  min-height: 60px;
-}
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
-}
-</style>
